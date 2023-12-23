0f578363e163c149b2a1e1fd2743a7096a05f8ce
wangyouwei2016 <13709821410@139.com>
2023-12-23 20:26:18 +0800

    Update UserController.java

--- src/test/java/com/example/webframework/UserController.java
+++ src/test/java/com/example/webframework/UserController.java
@@ -1,17 +1,47 @@
-// UserController.java
-package com.example.demo;
+package com.journaldev.examples;
+import java.io.IOException;
+import java.sql.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {}
+    }
 
-@RestController
-public class UserController {
-
-    @GetMapping("/user")
-    public String getUserInfo() {
-        // Insecure direct object reference vulnerability
-        // This endpoint should only be accessible to authenticated users with specific roles
-        // However, it currently allows any user to access sensitive user information
-        return "Sensitive user information";
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        boolean success = false;
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        // Unsafe query which uses string concatenation
+        String query = "select * from tbluser where username='" + username + "' and password = '" + password + "'";
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/user", "root", "root");
+            stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            if (rs.next()) {
+                // Login Successful if match is found
+                success = true;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                stmt.close();
+                conn.close();
+            } catch (Exception e) {}
+        }
+        if (success) {
+            response.sendRedirect("home.html");
+        } else {
+            response.sendRedirect("login.html?error=1");
+        }
     }
 }
